43ffa5bbbbf6e9a0ab2bdb5068fac9601c5c69b6
Jim Thomason <jim@japanesewartuba.com>
2019-02-08 14:18:29 -0600

    Updates

---
+++ precache-manifest.2fcf20221ff0bb9f9084d69c0d0a7b27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad10c30a3bc5b1a2e2dd",
+    "url": "/synchronous-sagas/static/js/runtime~main.ad10c30a.js"
+  },
+  {
+    "revision": "49ff6efab962aa1c5177",
+    "url": "/synchronous-sagas/static/js/main.49ff6efa.chunk.js"
+  },
+  {
+    "revision": "14431054da205b8df527",
+    "url": "/synchronous-sagas/static/js/1.14431054.chunk.js"
+  },
+  {
+    "revision": "49ff6efab962aa1c5177",
+    "url": "/synchronous-sagas/static/css/main.bfb83bdb.chunk.css"
+  },
+  {
+    "revision": "03b5662e53b99d2c21ff6bad81b2eda2",
+    "url": "/synchronous-sagas/index.html"
+  }
+];
